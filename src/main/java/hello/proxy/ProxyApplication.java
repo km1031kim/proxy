a96c66f095361644f0c909bb81b5e5f4c4e8443d
km1031kim@naver.com <km1031kim@naver.com>
2026-04-04 18:21:06 +0900

    proxy pattern

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -1,12 +1,17 @@
 package hello.proxy;
 
 import hello.proxy.config.AppV1Config;
+import hello.proxy.config.AppV2Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import(AppV1Config.class)
-@SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
+/**
+ * Import - 클래스를 스프링 빈으로 등록
+ */
+//@Import(AppV1Config.class)
+@Import({AppV1Config.class, AppV2Config.class})
+@SpringBootApplication(scanBasePackages = "hello.proxy.app") // 컴포넌트 스캔의 베이스 위치
 public class ProxyApplication {
 
 	public static void main(String[] args) {
